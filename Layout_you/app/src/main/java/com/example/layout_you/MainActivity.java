a50be0d54485269727e54f07a0a831d7388000a4
Ramsay2 <sachin114sharma@gmail.com>
2021-06-29 16:47:38 +0530

    Layout You

---
+++ Layout_you/app/src/main/java/com/example/layout_you/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.layout_you;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTvSaveInstance;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTvSaveInstance = findViewById(R.id.tvSaveInstance);
+        mTvSaveInstance.setText("Hey Masai");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("key","screen rotated");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String data = savedInstanceState.getString("key");
+        Log.d("sharma","restore instance");
+        mTvSaveInstance.setText(data);
+    }
+}
